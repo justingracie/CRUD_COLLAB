8b82640827f31c71a87a65a68ab8536630753e05
justingracie <j.m.gracie@gmail.com>
2022-04-20 12:43:24 -0600

    setup database

---
+++ models/menu.js
@@ -0,0 +1,19 @@
+const menu = [
+    {
+        item: 'ravioli' ,
+        image: 'https://media.istockphoto.com/photos/ravioli-with-tomato-sauce-and-basil-picture-id478215884?k=20&m=478215884&s=612x612&w=0&h=J9rkz3jxCJaQHG4IUiLWHJUQ39v914GPVxkJfnK22wQ=',
+    
+        price: 25
+    
+    },
+    {
+        item: 'pizza',
+        image: 'https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVwcGVyb25pJTIwcGl6emF8ZW58MHx8MHx8&w=1000&q=80',
+        price: 20
+    }
+    
+    
+    
+    ];
+    
+    module.exports = menu;
---
+++ server.js
@@ -0,0 +1,11 @@
+const menu = require('./models/menu');
+
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+
+app.listen(PORT, ()=>
+    console.log(`listening on ${PORT}`)
+);
