0518bbcca63b2abef4df076149da333ae99c00ee
justingracie <j.m.gracie@gmail.com>
2022-04-20 15:31:04 -0600

    added server code for edit and Delete

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const menu = require('./models/menu');
 
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 app.set('view engine', 'ejs');
 const PORT = 4000;
-app.use(express.urlencoded({extended: false}));
 
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 app.use(express.static('public'))
 
 // =====================
@@ -25,6 +27,13 @@ app.get('/menu/new', (req,res) =>{
     res.render('new.ejs');
 })
 
+//Delete/Destroy Route ---->
+
+app.delete('menu/:id', (req, res) =>{
+    const id = req.params.id
+    menu.splice(id, 1);
+    res.redirect('/menu');
+});
 
 // Post a new Input ----->
 
@@ -32,7 +41,24 @@ app.post('/menu', (req, res) =>{
     //let newItem = req.body
     menu.unshift(req.body);
     res.redirect('/menu');
-})
+});
+
+//Update Route ----->
+
+app.put('/menu/:id', (req,res) =>{
+    menu[req.params.id] = req.body
+    res.redirect(`/menu/${req.params.id}`); 
+});
+
+// Edit Route ----->
+app.get('/menu/:id/edit', (req,res) =>{
+    const menuUpdate = menu[req.params.id]
+    const context = {
+        menu: menuUpdate,
+        id: req.params.id
+    }
+    res.render('edit.ejs', context);
+});
 
 //Show Route ----->
 
@@ -43,13 +69,6 @@ app.get('/menu/:id', (req,res) =>{
     }
     res.render('show.ejs', context);
 });
-
-
-
-
-
-
-
 
 
 
