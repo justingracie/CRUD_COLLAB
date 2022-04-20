b821a47fb88b3ed6e131d8ea4bb5613b831d479b
justingracie <j.m.gracie@gmail.com>
2022-04-20 13:23:46 -0600

    Index and show routes working

--- server.js
+++ server.js
@@ -2,8 +2,37 @@ const menu = require('./models/menu');
 
 const express = require('express');
 const app = express();
-
+app.set('view engine', 'ejs');
 const PORT = 4000;
+
+
+// =====================
+//    ROUTES
+// =====================
+
+//home/index Route----->
+
+app.get('/menu', (req,res)=> {
+    const context = {menu: menu}
+    res.render('index.ejs', context);
+})
+
+//Show Route ----->
+
+app.get('/menu/:id', (req,res) =>{
+    let menuId = req.params.id
+    const context ={
+        menuItem: menu[menuId]
+    }
+    res.render('show.ejs', context);
+});
+
+
+
+
+
+
+
 
 
 
